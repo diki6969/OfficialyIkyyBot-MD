b4b51b454c5e72f7e7efab4456e20c0078aa01b8
diki6969 <gendonmenjeng@gmail.com>
2023-01-13 13:15:10 +0700

    .

--- help.js
+++ help.js
@@ -177,7 +177,8 @@ return`
  ${no++} ➳ ${prefix}base64
  ${no++} ➳ ${prefix}debase32
  ${no++} ➳ ${prefix}debase64
- $(no++) ➳ $(prefix)aidraw
+ $(no++) ➳ ${prefix}aidraw
+ $(no++) ➳ ${prefix}simi
 
  𝘽𝙐𝙂𝙑𝙄𝙋 𝙈𝙀𝙉𝙐 
  ${no++} ➳ ${prefix}sendbug 628xxx
